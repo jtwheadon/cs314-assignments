bdbcc8f8179d3d6244896cfe667f0305eea5f1cf
Jeff Wheadon <jwheadon@zagmail.gonzaga.edu>
2018-04-02 19:55:44 -0700

    started project

--- index.js
+++ index.js
@@ -1,83 +1,86 @@
-//TO RUN STEP BY STEP, uncomment the step you would like to see
-//step4();
-//step5();
-//step7();
-//steps11through14();
-//console.log(step15("Yo","hey","whatsup"));
-//console.log(step16("Yo","hey","whatsup"));
-//console.log(step17("Yo","hey","whatsup"));
-
-function step4() {
-  let ans = confirm("Press OK if you like apples");
-  if (ans == true) {
-    alert("I like Apples Too");
-  } else {
-    alert("Thats too bad");
-  }
-}
-
-function step5() {
-  let ans = confirm("Press OK if you like apples");
-  alert(ans ? "I like Apples Too" : "Thats too bad");
-}
-
-function step7() {
-  let age = prompt("How old are you?");
-
-  if (age == null || age == "") {
-    alert("You didn't give me your age....");
-  }
-  else if(isNaN(age)){
-    alert("That is not a number")
-  }
-  else {
-    alert("Thanks for the data");
-  }
-}
 
 
-function User(firstname, lastname, username) {
-  this.firstName = firstname;
-  this.lastName = lastname;
-  this.username = username;
-  this.fullname = function() {return this.firstName + " " + this.lastName;};
-}
+/*
+1. Char Swap
 
-function copyUser(user) {
-  let newUser = new User(user.firstName, user.lastName, user.username);
-  let ans = confirm("Enter your favorite color to enter into our raffle");
-  if (ans == true) {
-    let color = prompt("What is your favorite color?");
-    if (color == null || color == ""){
-      alert("You did not qualify for the raffle");
-    }
-    else {
-      newUser.favColor = color;
-      alert("Thanks for entering the raffle");
-    }
-  }
-  alert("New User Created");
-  return(newUser);
-}
+Write a JavaScript function to create a new string from an input string from
+the user swapping the position of first and last characters. The string length entered by
+the user must be greater than or equal to 1.
+*/
 
-function steps11through14() {
-  let user = new User("Jeff", "Wheadon", "jwheadon");
-  let newUser = copyUser(user);
+/*
+2. Hi String
 
-  console.log("User: ",user);
-  console.log("Copy of User: ",newUser);
-  console.log("Users full name: " + user.fullname());
-  console.log("Copy of User's full name: " + newUser.fullname());
-}
+Write a JavaScript function that asks for a string from the user and returns a
+new string adding "Hi" in front of the input string. If the input string begins with "Hi" then
+return the original string. The string length entered by the user must be greater than or
+equal to 1.
+*/
 
-function step15(string1, string2, string3="Hello") {
-  return "String 1: " + string1 + "\nString 2: " + string2 + "\nString 3: " + string3;
-}
+/*
+3. Three Chars to Front
 
-let step16 = function(string1, string2, string3="Hello") {
-  return "String 1: " + string1 + "\nString 2: " + string2 + "\nString 3: " + string3;
-}
+Write a JavaScript function to create a new string from an input
+string taking the last 3 characters and adding them to the front of the string. The string
+length entered by the user must be at least 3 characters long.
+*/
 
-let step17 = (string1, string2, string3="Hello") => {
-  return "String 1: " + string1 + "\nString 2: " + string2 + "\nString 3: " + string3;
-};
+/*
+4. Strings to Sentence
+
+Write a JavaScript function that asks for a list of items from the
+user separated by commas. Convert this to an array without the commas or extra
+spaces. Alert the items back to the user with a single saying that is formed using
+backticks (``).
+Example:
+input from user: blue, dogs, chocolate
+output from function: My favorite color is blue, dogs are awesome, and I love chocolate!
+*/
+
+/*
+5. Upper or Lower
+
+Write a JavaScript function that asks for a string from the user and
+creates a new string from that string where the first 3 characters are lowercase. If the
+string length entered by the user is less than 3 convert all the characters in upper case.
+*/
+
+/*
+6. Integer Swap
+
+Write a JavaScript function that asks for a comma separated list of
+numbers from the user and swap the first and last elements of a given array of integers.
+Alert the result to the user. The array length given from the user should be at least 1.
+*/
+
+/*
+7. Longest String
+
+Write a JavaScript function that asks for a comma separated list of
+strings from the user and alerts the longest string from the given array of strings.
+*/
+
+/*
+8. Largest Even Number
+
+Write a JavaScript function that asks for a comma separated list
+of numbers from the user. Convert this list to an array of numbers and alert the largest
+even number from the array of integers.
+*/
+
+/*
+9. Current Day Time
+
+Write a JavaScript function that alerts the current day and time in the
+following format:
+Example:
+Today is Friday.
+It is 4:00PM.
+*/
+
+/*
+10. Unlimited Function
+
+Write a JavaScript function that accepts an unlimited number of
+arguments and prints them out in a single string in a single alert.
+*/
